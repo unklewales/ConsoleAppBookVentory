ff9af0f609c7523d01f9976e2725aa4aff4b82cd
Unklewales <darexolu16@gmail.com>
2023-04-28 13:08:20 +0100

    Add project files.

---
+++ ConsoleAppBookVentory/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBookVentory
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Title: {Title}\nAuthor: {Author}\nPrice: {Price:C}\nQuantity in stock: {StockQuantity} pieces";
+        }
+    }
+}
---
+++ ConsoleAppBookVentory/Program.cs
@@ -0,0 +1,110 @@
+﻿using static System.Reflection.Metadata.BlobBuilder;
+
+namespace ConsoleAppBookVentory
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           IList<Book> books = new List<Book>()
+           {
+               new Book 
+               {
+                Title = "Awakenings",
+                Author = "H.Olivia",
+                Price = 10m, 
+                StockQuantity= 1,
+               },
+
+               new Book
+               {
+                Title = "The Matarese Circle",
+                Author = "R.Ludlum",
+                Price = 16m,
+                StockQuantity= 25,
+               },
+
+               new Book
+               {
+                Title = "Lord of the Rings",
+                Author = "J.Tolkien",
+                Price = 20m,
+                StockQuantity= 15,
+               },
+
+               new Book
+               {
+                Title = "Ogboju ode ninu igbo irunmole",
+                Author = "D.O. Fagunwa",
+                Price = 21m,
+                StockQuantity= 0,
+               },
+
+               new Book
+               {
+                Title = "The Bourne Supremacy",
+                Author = "R.Ludlum",
+                Price = 23m,
+                StockQuantity= 7,
+               },
+           };
+            
+            ViewAllBooks(books);
+            ViewBooksByAuthor(books, "R.Ludlum");
+            ViewBooksByPrice(books, 10m, 20m);
+            ViewOutOfStockBooks(books);
+        }
+
+        public static void ViewAllBooks(IList<Book> book)
+        {
+            Console.WriteLine("***All books in Collection***");
+            Console.WriteLine();
+            var allBooks = book.Where(book => book.Title != null && book.Author != null && book.Price != null && book.StockQuantity != null).ToList();
+
+            foreach (var items in allBooks)
+            {
+               Console.WriteLine(items);
+                Console.WriteLine();
+            }
+        }
+
+       public static void ViewBooksByAuthor(IList<Book> book, string author)
+       {
+            Console.WriteLine();
+            Console.WriteLine("****Books by Same Author****");
+            Console.WriteLine();
+            var allBooksByAuthor = book.Where(book => book.Author == author);
+            foreach (var item in allBooksByAuthor)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
+
+        }
+
+       public static void ViewBooksByPrice(IList<Book> book, decimal minprice, decimal maxprice)
+       {
+            Console.WriteLine($"***Books within the Range of the prices {minprice}-{maxprice}****");
+            Console.WriteLine();
+            var BooksWithinPriceRange = book.Where(book => book.Price > minprice && book.Price < maxprice);
+            foreach (var item in BooksWithinPriceRange)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        public static  void ViewOutOfStockBooks(IList<Book> book)
+        {
+            Console.WriteLine();
+            Console.WriteLine("***Books out of Stock***");
+            Console.WriteLine();
+            var BooksOutStock = book.Where(book => book.StockQuantity == 0);
+            foreach (var item in BooksOutStock)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
